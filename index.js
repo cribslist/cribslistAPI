16a4a318cb02431777b427714e4c8f67e5aea8a6
Morgantheplant <morgantheplant@gmail.com>
2018-10-17 21:13:56 -0700

    add item/items routes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+const item = require('./public/item.json');
+const items = require('./public/items.json');
 
 express()
-  .use("/", express.static(__dirname + '/public'))
-  // .get('*', function(req, res){
-  //       req.redirect('')
-  //   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use('/', express.static(__dirname + '/public'))
+    .get('/item', (req, res) => res.json(item))
+    .get('/items', (req, res) => res.json(items))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
