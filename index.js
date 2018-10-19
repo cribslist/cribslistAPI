4350242034428a34c9f90f927b67e8568eb4997c
Sukwon Lee <slee@hearsaycorp.com>
2018-10-18 19:43:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const PORT = process.env.PORT || 5000;
 
 const item = require('./public/item.json');
 const items = require('./public/items.json');
+const account = require('./public/account.json');
 
 express()
     .use('/', express.static(__dirname + '/public'))
     .get('/item', (req, res) => res.json(item))
     .get('/items', (req, res) => res.json(items))
+    .get('/account', (req, res) => res.json(account))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
