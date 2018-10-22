ceec07006a9b858eb65558c26f1054f3782cf466
Morgantheplant <morgantheplant@gmail.com>
2018-10-21 18:38:38 -0700

    add item id

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const account = require('./public/account.json');
 express()
     .use('/', express.static(__dirname + '/public'))
     .get('/item', (req, res) => res.json(item))
+    .get('/item/:userId', (req, res) => res.json(item))
     .get('/items', (req, res) => res.json(items))
     .get('/account', (req, res) => res.json(account))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
